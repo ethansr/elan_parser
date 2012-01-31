7e844176c705b58190440c4ed68cb24bb293fda2
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-01-31 12:21:31 -0800

    seperate header from annotation document

--- lib/elan_parser/xml.rb
+++ lib/elan_parser/xml.rb
@@ -4,7 +4,9 @@ require "happymapper"
 module ElanParser
 	module XML
     autoload :AnnotationDocument, 'elan_parser/xml/annotation_document'
+    autoload :Header, 'elan_parser/xml/header'
     autoload :MediaDescriptor, 'elan_parser/xml/media_descriptor'
     autoload :Property, 'elan_parser/xml/property'
+    autoload :LinkedFileDescriptor, 'elan_parser/xml/linked_file_descriptor'
 		end
 end
--- lib/elan_parser/xml/annotation_document.rb
+++ lib/elan_parser/xml/annotation_document.rb
@@ -1,19 +1,6 @@
 module ElanParser
   module XML
 
-		class Header
-			include HappyMapper
-
-			tag 'HEADER'
-
-			#Deprecated in favor of media_descriptors
-			attribute :media_file, String, :tag => 'MEDIA_FILE'
-			attribute :time_units, String, :tag => 'TIME_UNITS'
-
-			has_many :media_descriptors, MediaDescriptor
-			has_many :linked_file_descriptors, LinkedFileDescriptor
-			has_many :properties, Property
-		end
 
 		class TimeSlot
 			include HappyMapper
