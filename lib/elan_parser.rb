ffa2d9ae34eb553471aa70855c76afdbaea72eac
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-01-31 11:47:05 -0800

    break down files for parsing more

---
+++ lib/elan_parser.rb
@@ -0,0 +1,7 @@
+require "elan_parser/version"
+
+
+module ElanParser
+  autoload :XML,            'elan_parser/xml'
+  autoload :DB,             'elan_parser/elan_parser_db_model'
+end
---
+++ lib/elan_parser/xml.rb
@@ -0,0 +1,8 @@
+require "elan_parser/version"
+require "happymapper"
+
+module ElanParser
+	module XML
+    autoload :AnnotationDocument, 'elan_parser/xml/annotation_document'
+		end
+end
